9492351e3d531a30cce033e09899fa696ee6c337
kezia-hansen <131632957+kezia-hansen@users.noreply.github.com>
2024-02-01 12:08:11 +0100

    lavet opgaver

---
+++ parameters/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// let myName = "Frankie";
+
+let firstName = "Mona";
+
+// let lastName = "Jensen";
+
+let animalName = "koko";
+
+let animalType = "cow";
+
+// sayHello();
+
+/* function sayHello() {
+  console.log(`Hello ${firstName} ${lastName}`);
+} */
+
+presentPet("Bob", animalType, animalName);
+function presentPet(firstName, animalType, animalName) {
+  console.log(`My name is ${firstName}, I have a ${animalType} called ${animalName}`);
+}
