58bdc87cee1755335424322b5cac317196b3a322
kezia-hansen <131632957+kezia-hansen@users.noreply.github.com>
2024-02-02 10:06:18 +0100

    lavet øvelse til rektangel

--- udregnrektangel/script.js
+++ udregnrektangel/script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+udregnRektangelAreal();
+
+function udregnRektangelAreal(width, height) {
+  return width * height;
+}
+
+let beregnetAreal = udregnRektangelAreal(30, 10);
+console.log("beregnetAreal", beregnetAreal);
