014b0468d7484a1faf0144ff6d09dbb7c7d4573e
kezia-hansen <131632957+kezia-hansen@users.noreply.github.com>
2024-02-02 10:48:16 +0100

    lavet omregn areal i m² til areal i ft²

---
+++ omregn_m²_ft²/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+convertSquareMToSquareFt();
+
+function convertSquareMToSquareFt(m2) {
+  return m2 * 10.76391042;
+}
+
+let beregnetft2 = convertSquareMToSquareFt(400);
+console.log("beregnetft2", beregnetft2);
