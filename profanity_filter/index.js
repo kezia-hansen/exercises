d418d4627f4590f5301c5690c21868c4a6076998
kezia-hansen <131632957+kezia-hansen@users.noreply.github.com>
2024-02-12 05:03:21 +0100

    påbegyndt profanity filter

--- profanity_filter/index.js
+++ profanity_filter/index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+/* //Semi spoiler alert profanity prep 1: Find et ord i en streng og udskift det
+
 let theText = "Jeg er en tekst hvor nogle ord skal skiftes ud med nogle andre ord";
 
 // Using split/join
@@ -9,3 +11,27 @@ console.log(newWord);
 // Using replaceAll
 let newText = theText.replaceAll("ord", "ting");
 console.log(newText);
+
+//*************************************************/
+
+const curseWords = [
+  { bad: "var", good: "const" },
+  { bad: "float", good: "grid" },
+  { bad: "marquee", good: "just don't" },
+];
+
+const btn = document.querySelector(".sfw-btn");
+
+function replaceCursedWords() {
+  let newText = document.querySelector("p").textContent;
+  curseWords.forEach((word) => {
+    newText = newText.replaceAll(word.bad, word.good);
+  });
+
+  document.querySelector("p").textContent = newText;
+}
+
+btn.addEventListener("click", () => {
+  replaceCursedWords();
+  console.log("updated");
+});
