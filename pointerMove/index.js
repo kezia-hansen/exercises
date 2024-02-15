587d6a768c92e6cffb08cb56f827ad3aa952ca4b
kezia-hansen <131632957+kezia-hansen@users.noreply.github.com>
2024-02-15 11:05:58 +0100

    PointerEvent done

---
+++ pointerMove/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const body = document.querySelector("body");
+document.addEventListener("mousemove", skiftFarve);
+
+function skiftFarve(evt) {
+  const i = (evt.clientX / window.innerWidth) * 100;
+  body.style.setProperty("--i", `${i}%`);
+}
