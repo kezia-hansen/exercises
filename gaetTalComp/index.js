e948ea9cb2cdc88824e5fec71be74affcf2e8b76
kezia-hansen <131632957+kezia-hansen@users.noreply.github.com>
2024-02-16 07:46:49 +0100

    semi done

--- gaetTalComp/index.js
+++ gaetTalComp/index.js
@@ -12,9 +12,13 @@ document.querySelector("#forlav").addEventListener("mousedown", tooLowOrHi);
 document.querySelector("#forhoej").addEventListener("mousedown", tooLowOrHi);
 document.querySelector("#rigtig").addEventListener("mousedown", tooLowOrHi);
 document.querySelector("#start").addEventListener("mousedown", startGame);
+document.querySelector("#startOver").addEventListener("mousedown", startOver);
 
 function startGame() {
   numberOfGuesses = 0;
+  lowNum = 0;
+  highNum = 100;
+  computerGuess = Math.floor(Math.random() * (highNum - lowNum + 1)) + lowNum;
   computerGuessText.textContent = `Tænk på et tal mellem 0-100`;
   image.src = "bearstart.png";
   computerGuesses();
@@ -37,6 +41,11 @@ function tooLowOrHi(evt) {
     highNum = computerGuess;
     image.src = "bearhigh.png";
   } else {
+    if (computerGuess === userNum) {
+      computerGuessText.textContent = `Jeg gættede rigtigt! Det var ${computerGuess}.`;
+      image.src = "bearright.png";
+      return;
+    }
     computerGuess = userNum;
     image.src = "bearright.png";
   }
